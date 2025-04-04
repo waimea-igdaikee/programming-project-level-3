afcdd2dea29205d70a4b733d43a27e9e9d6a942a
waimea-igdaikee <168692114+waimea-igdaikee@users.noreply.github.com>
2025-04-04 15:07:04 +1300

    Keylisteners not working!!!!

--- src/Main.kt
+++ src/Main.kt
@@ -36,6 +36,7 @@ class App() {
     var currentLocation: Scene = entrance
     val inventory = mutableListOf<Item>()
 
+
     // Application logic functions
     fun move(dir: Char) {
         when (dir) {
@@ -101,11 +102,30 @@ val hallway114 = Hallway(Triple(1,1,4))
 val hallway115 = Hallway(Triple(1,1,5))
 val hallway215 = Hallway(Triple(2,1,5))
 val weaponsRoom = Scene(Triple(3,1,2), "Weapons Room", "A open room, shelves lines with weapons")
-val elevator1 = Scene(Triple(3,1,5), "Elevator", "An untrustworthy elevator at the top")
-val elevator2 = Scene(Triple(3,2,5), "Elevator", "An untrustworthy elevator at the bottom")
+val elevator315 = Scene(Triple(3,1,5), "Elevator", "An untrustworthy elevator at the top")
 val messHall = Scene(Triple(4,1,4), "Mess Hall", "An untidy mess hall, with flickering lights")
 val computerRoom = Scene(Triple(5,1,3), "Computer Room", "A computer room with seemingly outdated technology")
 val serverRoom = Scene(Triple(5,1,2), "Server Room", "A server room, fans spinning and lights blinking")
+val elevator325 = Scene(Triple(3,2,5), "Elevator", "An untrustworthy elevator at the bottom")
+val hallway425 = Hallway(Triple(4,2,5))
+val hallway525 = Hallway(Triple(5,2,5))
+val hallway625 = Hallway(Triple(6,2,5))
+val hallway624 = Hallway(Triple(6,2,4))
+val hallway623 = Hallway(Triple(6,2,3))
+val hallway423 = Hallway(Triple(4,2,3))
+val storageRoom526 = Scene(Triple(5,2,6), "Storage Room", "A dank storage room")
+val storageRoom622 = Scene(Triple(6,2,2), "Storage Room", "A smelly storage room")
+val controlRoom = Scene(Triple(4,2,4),"Control Room", "A control room with an ancient computer whirring in the centre")
+val generatorRoom = Scene(Triple(3,2,3),"Generator Room", "An open hall with huge diesel generators")
+val elevator724 = Scene(Triple(7,2,4), "Elevator", "A sharp ledge to an empty elevator shaft. There's some dangling wires you could climb down - if you dared")
+val labRoom = Scene(Triple(5,2,3), "Lab Room", "A chemistry lab with abandoned experiments in petri dishes and beakers. There are some jerry cans in the corner.")
+
+val elevator734 = Scene(Triple(7,3,4), "Elevator", "The bottom of an empty elevator shaft")
+val hallway634 = Hallway(Triple(6,3,4))
+val hallway633 = Hallway(Triple(6,3,3))
+val hallway635 = Hallway(Triple(6,3,5))
+val portalRoom = Scene(Triple(5,3,5), "Portal Chamber", "The frame of what looks to be a portal. It is inscribed with glowing runes.")
+val portalControlRoom = Scene(Triple(5,3,3), "Portal Controls", "A set of levers and buttons that seems to control the portal.")
 
 
 open class Scene(open val location: Triple<Int, Int, Int>, val name: String? = null, val description: String? = null) {
@@ -169,9 +189,11 @@ class Item(val name: String, val spawnLocations: Array<Scene>, val type: String,
 
 
 // Initialise items
-val labKey = Item("Green Keycard", arrayOf(blastDoor), "Key", 1)
-
+val facilityKey = Item("Green Keycard", arrayOf(blastDoor), "Key", 1)
 val weaponRoomKey = Item("Blue Keycard", arrayOf(messHall, serverRoom), "Key", 2)
+val labKey = Item("Orange Keycard", arrayOf(storageRoom526, storageRoom622), "Key", 3)
+
+val jerryCan = Item("Jerry Can", arrayOf(labRoom), "Generic", 4)
 
 
 fun main() {
@@ -190,17 +212,43 @@ fun main() {
     hallway115.addToMap()
     hallway215.addToMap()
     weaponsRoom.addToMapLocked(2)
-    elevator1.addToMap()
-    elevator1.enableVerticalConnection('d')
+    elevator315.addToMap()
+    elevator315.enableVerticalConnection('d')
     messHall.addToMap()
     computerRoom.addToMap()
     serverRoom.addToMap()
-    elevator2.addToMap()
-    elevator2.enableVerticalConnection('u')
+
+
+    elevator325.addToMap()
+    elevator325.enableVerticalConnection('u')
+    hallway425.addToMap()
+    hallway525.addToMap()
+    hallway625.addToMap()
+    hallway624.addToMap()
+    hallway623.addToMap()
+    hallway423.addToMap()
+    storageRoom526.addToMap()
+    storageRoom622.addToMap()
+    controlRoom.addToMap()
+    generatorRoom.addToMap()
+    elevator724.addToMap()
+    elevator724.enableVerticalConnection('d')
+    labRoom.addToMapLocked(3)
+
+    elevator734.addToMap()
+    elevator734.enableVerticalConnection('u')
+    hallway634.addToMap()
+    hallway633.addToMap()
+    hallway635.addToMap()
+    portalRoom.addToMap()
+    portalControlRoom.addToMapLocked(2)
 
     // Initialise Items
-    labKey.spawn()
+    facilityKey.spawn()
     weaponRoomKey.spawn()
+    labKey.spawn()
+
+    jerryCan.spawn()
 
 }
 
@@ -209,7 +257,8 @@ fun main() {
  * Defines the UI and responds to events
  * The app model should be passed as an argument
  */
-class MainWindow(val app: App) : JFrame(), ActionListener {
+class MainWindow(val app: App) : JFrame(), ActionListener, KeyListener {
+
 
     // Fields to hold the UI elements
     private lateinit var titleLabel: JLabel
@@ -286,6 +335,8 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
         val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 36)
         val smallFont = Font(Font.SANS_SERIF, Font.PLAIN, 16)
 
+        this.addKeyListener(this)
+
 
         val DESCRIPTION_X = 35
         val DESCRIPTION_Y = 0
@@ -305,8 +356,7 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
         descriptionLabel.verticalAlignment = SwingConstants.TOP
         descriptionLabel.bounds = Rectangle(DESCRIPTION_X, DESCRIPTION_Y + 80, DESCRIPTION_WIDTH, 120)
         descriptionLabel.font = smallFont
-        descriptionLabel.foreground = Color.BLACK
-        descriptionLabel.background = Color.LIGHT_GRAY
+        descriptionLabel.background = Color(75, 80, 82)
         descriptionLabel.isOpaque = true
         add(descriptionLabel)
 
@@ -373,9 +423,9 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
         val inventoryLabels = arrayOf(inventoryLabel1, inventoryLabel2, inventoryLabel3, inventoryLabel4, inventoryLabel5)
 
         inventoryLabels.forEachIndexed() {index, label ->
-            label.horizontalAlignment = SwingConstants.LEFT
+            label.horizontalAlignment = SwingConstants.RIGHT
             label.bounds = Rectangle(
-                INVENTORY_X + 200,
+                INVENTORY_X + 140,
                 100 + INVENTORY_Y + (INVENTORY_BUTTON_SIZE + INVENTORY_SPACING) * index,
                 150,
                 INVENTORY_BUTTON_SIZE)
@@ -392,9 +442,9 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
         val itemLabels = arrayOf(itemLabel1, itemLabel2, itemLabel3, itemLabel4, itemLabel5)
 
         itemLabels.forEachIndexed() {index, label ->
-            label.horizontalAlignment = SwingConstants.LEFT
+            label.horizontalAlignment = SwingConstants.RIGHT
             label.bounds = Rectangle(
-                INVENTORY_X,
+                INVENTORY_X - 60,
                 100 + INVENTORY_Y + (INVENTORY_BUTTON_SIZE + INVENTORY_SPACING) * index,
                 150,
                 INVENTORY_BUTTON_SIZE)
@@ -472,16 +522,19 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
      */
     fun updateView() {
         titleLabel.text = app.currentLocation.name
-        descriptionLabel.text = app.currentLocation.description
+        descriptionLabel.text = "<html>" + app.currentLocation.description + "</html>" // Wrap in html tags to enable line wrapping
         // I should probably store all these in an array and then use .forEach()
         itemLabel1.text = app.getSceneItem(1)?.name ?: ""
         itemLabel2.text = app.getSceneItem(2)?.name ?: ""
+        itemLabel3.text = app.getSceneItem(3)?.name ?: ""
+        itemLabel4.text = app.getSceneItem(4)?.name ?: ""
+        itemLabel5.text = app.getSceneItem(5)?.name ?: ""
 
         inventoryLabel1.text = app.getInventoryItem(1)?.name ?: ""
         inventoryLabel2.text = app.getInventoryItem(2)?.name ?: ""
-//        inventoryLabel3.text = app.currentLocation?.printItem(3)
-//        inventoryLabel4.text = app.currentLocation?.printItem(4)
-//        inventoryLabel5.text = app.currentLocation?.printItem(5)
+        inventoryLabel3.text = app.getInventoryItem(3)?.name ?: ""
+        inventoryLabel4.text = app.getInventoryItem(4)?.name ?: ""
+        inventoryLabel5.text = app.getInventoryItem(5)?.name ?: ""
 
         if (app.currentLocation.adjacentScene('n') != null) {
             northButton.text = "↑"
@@ -557,6 +610,26 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
             dropButton5 -> app.dropItem(5)
         }
         updateView()
+    }
+
+    override fun keyTyped(e: KeyEvent?) {
+    }
+
+    override fun keyPressed(e: KeyEvent?) {
+        println("Down: ${e?.keyCode}")
+
+        // Check which key was pressed and act upon it
+        when (e?.keyCode) {
+            KeyEvent.VK_UP    -> print("Vdown")
+            KeyEvent.VK_DOWN  -> print("Vup")
+        }
+
+        // Ensure view matched the updated app model data
+        updateView()
+    }
+
+    override fun keyReleased(e: KeyEvent?) {
+
     }
 
 }
